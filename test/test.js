e050e3fbe0ad5315ffc0de4d5aa15f11b4fb8c43
patelotech <pj.patriciojulian@gmail.com>
2019-02-16 14:26:52 +0000

    monetization cta

--- test/test.js
+++ test/test.js
@@ -2,12 +2,20 @@ const assert = require('assert');
 
 const EventEmitter = require('events');
 const Agent = require('../lib/index.js');
+const linearAlgebra = require('linear-algebra')();
 
+const Matrix = linearAlgebra.Matrix;
 
 
 
 const jorgito = new Agent('jorgito');
 const juana = new Agent('juana');
+
+const m = new Matrix([
+  [1, 3, 3],
+  [0.3, 1, 3],
+  [0.3, 0.3, 1]
+]);
 
 
 
@@ -41,6 +49,7 @@ describe('EmitbutDeath2', () => {
 
 
 
+
 describe('StartJuana', () => {
   it('It should retrieve a true state.', () => {
 	console.log(juana.start());
@@ -55,10 +64,12 @@ describe('EmitandAlive', () => {
   });
 });
 
-describe('EmitandAlive', () => {
-  it('Juan should return the message', () => {
-  console.log('JORGITO: Hola Juana');
-    assert.equal('test', 'test');
+describe('AHP Test', () => {
+  it('no error', () => {
+      console.log(m);
+  jorgito.decide('ahp', m);
+  console.log(jorgito.decide('ahp', m));
+  assert.equal('test', 'test');
   });
 });
 
